c856aafc9903b233cb2f3ec179f93222cd1c6d34
Bruno Molina <brmolina@gmail.com>
2020-11-15 13:29:39 -0300

    Método adicionar verbo Http PUT com ResponseEntity ok E notFound

--- src/main/java/com/algaworks/algafood/api/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/CozinhaController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -55,5 +57,19 @@ public class CozinhaController {
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+		if(cozinhaAtual != null) {
+	//		cozinhaAtual.setNome(cozinha.getNome());
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		
+			cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
