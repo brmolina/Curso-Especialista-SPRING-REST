6c76a1615f15f5619519cb00efa551d340392c0a
Bruno Molina <brmolina@gmail.com>
2020-11-23 15:56:09 -0300

    Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.infrastructure.repository;
 
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,11 +16,14 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.ResultSetSupportingSqlParameter;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.repository.RestautanteRepositoryQueries;
 
 import lombok.var;
@@ -27,6 +33,9 @@ public class RestauranteRepositoryImpl implements RestautanteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired @Lazy
+	private RestauranteRepository restauranteRepository;
 	
 	@Override
 	public List<Restaurante> find(String nome, 
@@ -55,26 +64,12 @@ public class RestauranteRepositoryImpl implements RestautanteRepositoryQueries {
 		
 		return query.getResultList();
 		
-		/*
-		 * var jpql = new StringBuilder(); jpql.append("from Restaurante where 0=0 ");
-		 * 
-		 * var parametros = new HashMap<String, Object>();
-		 * 
-		 * if(StringUtils.hasLength(nome)) { jpql.append("and nome like :nome ");
-		 * parametros.put("nome", "%" + nome + "%"); }
-		 * 
-		 * if(taxaFreteInicial != null) { jpql.append("and taxaFrete >= :taxaInicial ");
-		 * parametros.put("taxaInicial", taxaFreteInicial); }
-		 * 
-		 * if(taxaFreteFinal != null) { jpql.append("and taxaFrete <= :taxaFinal ");
-		 * parametros.put("taxaFinal", taxaFreteFinal); }
-		 * 
-		 * TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(),
-		 * Restaurante.class);
-		 * 
-		 * parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		 * 
-		 * return query.getResultList();
-		 */
+	
+	}
+
+	@Override
+	public List<Restaurante> findComFreteGratis(String nome) {
+		
+		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
 	}
 }
