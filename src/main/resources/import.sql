25d08c51c993f5cc952d5143b9af30fc1df53e24
Bruno Molina <brmolina@gmail.com>
2020-11-24 11:27:06 -0300

    @CreationTimestamp e @UpdateTimestamp

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,8 +3,8 @@ insert into cozinha (id, nome) values (2,'Indiana');
 insert into cozinha (id, nome) values (3,'Americana');
 insert into cozinha (id, nome) values (4,'Italiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Burger Porn', 5.5, 3)
-insert into algafood.restaurante (nome, taxa_frete, cozinha_id) values ("Stilhano's Pizza", 7.50, 4)
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade_id) values ('Burger Porn', 5.5, 3, utc_timestamp, utc_timestamp, '08550-010', 'Rua Engenheiro de Brito', '100', 'Vila Julia', 3)
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ("Stilhano's Pizza", 7.50, 4, utc_timestamp, utc_timestamp)
 
 insert into forma_pagamento (descricao) values ('dinheiro')
 insert into forma_pagamento (descricao) values ('cartao')
@@ -16,6 +16,7 @@ insert into estado (id, nome) values (4, 'Goias')
 
 insert into cidade (nome, estado_id) values ('Campinas', 1)
 insert into cidade (nome, estado_id) values ('Ribeirao Preto', 1)
+insert into cidade (nome, estado_id) values ('Poá', 1)
 
 insert into cidade (nome, estado_id) values ('Cabo Frio', 2)
 insert into cidade (nome, estado_id) values ('Paraty', 2)
