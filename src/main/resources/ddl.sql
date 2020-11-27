ba918167a30cdf06d3dc17adb0cbe2182603c6e9
Bruno Molina <brmolina@gmail.com>
2020-11-27 11:14:00 -0300

    script javax.persistence migração V004 criacao varias tabelas

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,90 @@
+create table cidade (id bigint not null auto_increment, nome varchar(255), estado_id bigint, primary key (id)) engine=MyISAM
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=MyISAM
+create table estado (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=MyISAM
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255), primary key (id)) engine=MyISAM
+create table grupo (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=MyISAM
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=MyISAM
+create table permissao (id bigint not null auto_increment, descricao varchar(255), nome varchar(255), primary key (id)) engine=MyISAM
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=MyISAM
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, endereco_bairro varchar(255), endereco_cep varchar(255), endereco_complemento varchar(255), endereco_logradouro varchar(255), endereco_numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, endereco_cidade_id bigint, primary key (id)) engine=MyISAM
+create table restaurante_forma_pagamento (restaurante_id bigint not null, forma_pagamento_id bigint not null) engine=MyISAM
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=MyISAM
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=MyISAM
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKbc0tm7hnvc96d8e7e2ulb05yw foreign key (endereco_cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKa30vowfejemkw7whjvr8pryvj foreign key (restaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
+insert into cozinha (id, nome) values (1,'Tailandesa')
+insert into cozinha (id, nome) values (2,'Indiana')
+insert into cozinha (id, nome) values (3,'Americana')
+insert into cozinha (id, nome) values (4,'Italiana')
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade_id) values ('Burger Porn', 5.5, 3, utc_timestamp, utc_timestamp, '08550-010', 'Rua Engenheiro de Brito', '100', 'Vila Julia', 3)
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ("Stilhano's Pizza", 7.50, 4, utc_timestamp, utc_timestamp)
+insert into forma_pagamento (descricao) values ('dinheiro')
+insert into forma_pagamento (descricao) values ('cartao')
+insert into estado (id, nome) values (1, 'Sao Paulo')
+insert into estado (id, nome) values (2, 'Rio de Janeiro')
+insert into estado (id, nome) values (3, 'Minas Gerais')
+insert into estado (id, nome) values (4, 'Goias')
+insert into cidade (nome, estado_id) values ('Campinas', 1)
+insert into cidade (nome, estado_id) values ('Ribeirao Preto', 1)
+insert into cidade (nome, estado_id) values ('Poá', 1)
+insert into cidade (nome, estado_id) values ('Cabo Frio', 2)
+insert into cidade (nome, estado_id) values ('Paraty', 2)
+insert into cidade (nome) values ('Belo Horizonte')
+insert into cidade (nome) values ('Rio Quente')
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1), (1,2), (2,1)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Coca-cola', '350ml', 5.00, true, 1)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Pizza', 'Mussarela', 5.00, true, 2)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Hamburguer', 'Carne Bovina', 5.00, true, 1)
+create table cidade (id bigint not null auto_increment, nome varchar(255), estado_id bigint, primary key (id)) engine=MyISAM
+create table cozinha (id bigint not null auto_increment, nome varchar(255) not null, primary key (id)) engine=MyISAM
+create table estado (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=MyISAM
+create table forma_pagamento (id bigint not null auto_increment, descricao varchar(255), primary key (id)) engine=MyISAM
+create table grupo (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=MyISAM
+create table grupo_permissao (grupo_id bigint not null, permissao_id bigint not null) engine=MyISAM
+create table permissao (id bigint not null auto_increment, descricao varchar(255), nome varchar(255), primary key (id)) engine=MyISAM
+create table produto (id bigint not null auto_increment, ativo bit not null, descricao varchar(255) not null, nome varchar(255) not null, preco decimal(19,2) not null, restaurante_id bigint not null, primary key (id)) engine=MyISAM
+create table restaurante (id bigint not null auto_increment, data_atualizacao datetime not null, data_cadastro datetime not null, endereco_bairro varchar(255), endereco_cep varchar(255), endereco_complemento varchar(255), endereco_logradouro varchar(255), endereco_numero varchar(255), nome varchar(255) not null, taxa_frete decimal(19,2) not null, cozinha_id bigint not null, endereco_cidade_id bigint, primary key (id)) engine=MyISAM
+create table restaurante_forma_pagamento (restaurante_id bigint not null, forma_pagamento_id bigint not null) engine=MyISAM
+create table usuario (id bigint not null auto_increment, data_cadastro datetime not null, email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=MyISAM
+create table usuario_grupo (usuario_id bigint not null, grupo_id bigint not null) engine=MyISAM
+alter table cidade add constraint FKkworrwk40xj58kevvh3evi500 foreign key (estado_id) references estado (id)
+alter table grupo_permissao add constraint FKh21kiw0y0hxg6birmdf2ef6vy foreign key (permissao_id) references permissao (id)
+alter table grupo_permissao add constraint FKta4si8vh3f4jo3bsslvkscc2m foreign key (grupo_id) references grupo (id)
+alter table produto add constraint FKb9jhjyghjcn25guim7q4pt8qx foreign key (restaurante_id) references restaurante (id)
+alter table restaurante add constraint FK76grk4roudh659skcgbnanthi foreign key (cozinha_id) references cozinha (id)
+alter table restaurante add constraint FKbc0tm7hnvc96d8e7e2ulb05yw foreign key (endereco_cidade_id) references cidade (id)
+alter table restaurante_forma_pagamento add constraint FK7aln770m80358y4olr03hyhh2 foreign key (forma_pagamento_id) references forma_pagamento (id)
+alter table restaurante_forma_pagamento add constraint FKa30vowfejemkw7whjvr8pryvj foreign key (restaurante_id) references restaurante (id)
+alter table usuario_grupo add constraint FKk30suuy31cq5u36m9am4om9ju foreign key (grupo_id) references grupo (id)
+alter table usuario_grupo add constraint FKdofo9es0esuiahyw2q467crxw foreign key (usuario_id) references usuario (id)
+insert into cozinha (id, nome) values (1,'Tailandesa')
+insert into cozinha (id, nome) values (2,'Indiana')
+insert into cozinha (id, nome) values (3,'Americana')
+insert into cozinha (id, nome) values (4,'Italiana')
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro, endereco_cidade_id) values ('Burger Porn', 5.5, 3, utc_timestamp, utc_timestamp, '08550-010', 'Rua Engenheiro de Brito', '100', 'Vila Julia', 3)
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ("Stilhano's Pizza", 7.50, 4, utc_timestamp, utc_timestamp)
+insert into forma_pagamento (descricao) values ('dinheiro')
+insert into forma_pagamento (descricao) values ('cartao')
+insert into estado (id, nome) values (1, 'Sao Paulo')
+insert into estado (id, nome) values (2, 'Rio de Janeiro')
+insert into estado (id, nome) values (3, 'Minas Gerais')
+insert into estado (id, nome) values (4, 'Goias')
+insert into cidade (nome, estado_id) values ('Campinas', 1)
+insert into cidade (nome, estado_id) values ('Ribeirao Preto', 1)
+insert into cidade (nome, estado_id) values ('Poá', 1)
+insert into cidade (nome, estado_id) values ('Cabo Frio', 2)
+insert into cidade (nome, estado_id) values ('Paraty', 2)
+insert into cidade (nome) values ('Belo Horizonte')
+insert into cidade (nome) values ('Rio Quente')
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1), (1,2), (2,1)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Coca-cola', '350ml', 5.00, true, 1)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Pizza', 'Mussarela', 5.00, true, 2)
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Hamburguer', 'Carne Bovina', 5.00, true, 1)
