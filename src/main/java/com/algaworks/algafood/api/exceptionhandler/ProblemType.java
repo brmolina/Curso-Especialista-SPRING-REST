90015626e491a6195e18659ce33f0cb61aa66b38
Bruno Molina <brmolina@gmail.com>
2021-01-19 15:43:33 -0300

    tratando outras exceções não capturadas INTERNAL_SERVER_ERROR

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
-	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"), 
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"), 
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
